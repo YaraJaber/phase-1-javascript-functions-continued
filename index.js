4da9f92e5c6087f69a324a660e1fe3a3f8a28be3
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-02 14:24:03 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(job = "go to the office") {
+    return `This Monday, I will ${job}.`;
+  }
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+
+  const saturdayFunFunction = saturdayFun();
+console.log(saturdayFunFunction); 
+
+const customSaturdayFunFunction = saturdayFun("play soccer");
+console.log(customSaturdayFunFunction); 
+
+const mondayWorkFunction = mondayWork();
+console.log(mondayWorkFunction); 
+
+const customMondayWorkFunction = mondayWork("work from home");
+console.log(customMondayWorkFunction); 
+
+const wrapFlair = wrapAdjective("!!!");
+const wrappedAdjective = wrapFlair("awesome");
+console.log(wrappedAdjective);
